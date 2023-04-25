9e2b4884d29fdde1ae576795b940beb38a81f921
Bianca <bianbdecima@gmail.com>
2023-04-25 10:42:58 -0300

    Log in Form Design

--- AbrilClinic.Presentation/LogInForm.Designer.cs
+++ AbrilClinic.Presentation/LogInForm.Designer.cs
@@ -28,54 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_logIn = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.Label lbl_password;
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.tbx_username = new System.Windows.Forms.TextBox();
+            this.txb_password = new System.Windows.Forms.TextBox();
+            this.btn_logIn = new System.Windows.Forms.Button();
+            this.lbl_title1 = new System.Windows.Forms.Label();
+            this.lbl_title2 = new System.Windows.Forms.Label();
+            lbl_password = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lbl_logIn
+            // lbl_password
             // 
-            this.lbl_logIn.AutoSize = true;
-            this.lbl_logIn.Location = new System.Drawing.Point(107, 98);
-            this.lbl_logIn.Name = "lbl_logIn";
-            this.lbl_logIn.Size = new System.Drawing.Size(112, 15);
-            this.lbl_logIn.TabIndex = 0;
-            this.lbl_logIn.Text = "Nombre de usuario:";
+            lbl_password.AutoSize = true;
+            lbl_password.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_password.ForeColor = System.Drawing.Color.Black;
+            lbl_password.Location = new System.Drawing.Point(192, 254);
+            lbl_password.Name = "lbl_password";
+            lbl_password.Size = new System.Drawing.Size(80, 17);
+            lbl_password.TabIndex = 2;
+            lbl_password.Text = "Contraseña:";
             // 
-            // textBox1
+            // lbl_username
             // 
-            this.textBox1.Location = new System.Drawing.Point(115, 136);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 1;
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_username.ForeColor = System.Drawing.Color.Black;
+            this.lbl_username.Location = new System.Drawing.Point(192, 182);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(123, 17);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Nombre de usuario:";
             // 
-            // label1
+            // tbx_username
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(115, 197);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Contraseña:";
+            this.tbx_username.Location = new System.Drawing.Point(192, 202);
+            this.tbx_username.Name = "tbx_username";
+            this.tbx_username.Size = new System.Drawing.Size(219, 23);
+            this.tbx_username.TabIndex = 1;
             // 
-            // textBox2
+            // txb_password
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 237);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 3;
+            this.txb_password.Location = new System.Drawing.Point(192, 274);
+            this.txb_password.Name = "txb_password";
+            this.txb_password.Size = new System.Drawing.Size(219, 23);
+            this.txb_password.TabIndex = 3;
+            // 
+            // btn_logIn
+            // 
+            this.btn_logIn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btn_logIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_logIn.FlatAppearance.BorderSize = 0;
+            this.btn_logIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_logIn.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_logIn.ForeColor = System.Drawing.Color.White;
+            this.btn_logIn.Location = new System.Drawing.Point(192, 340);
+            this.btn_logIn.Name = "btn_logIn";
+            this.btn_logIn.Size = new System.Drawing.Size(219, 33);
+            this.btn_logIn.TabIndex = 4;
+            this.btn_logIn.Text = "Ingresar";
+            this.btn_logIn.UseVisualStyleBackColor = false;
+            // 
+            // lbl_title1
+            // 
+            this.lbl_title1.AutoSize = true;
+            this.lbl_title1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_title1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl_title1.Location = new System.Drawing.Point(129, 79);
+            this.lbl_title1.Name = "lbl_title1";
+            this.lbl_title1.Size = new System.Drawing.Size(367, 21);
+            this.lbl_title1.TabIndex = 5;
+            this.lbl_title1.Text = "Una cuenta, todos los servicios de Abril Clínica";
+            // 
+            // lbl_title2
+            // 
+            this.lbl_title2.AutoSize = true;
+            this.lbl_title2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_title2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_title2.Location = new System.Drawing.Point(115, 111);
+            this.lbl_title2.Name = "lbl_title2";
+            this.lbl_title2.Size = new System.Drawing.Size(397, 17);
+            this.lbl_title2.TabIndex = 6;
+            this.lbl_title2.Text = "Inicia seción para acceder a Servicios Online para Pacientes";
             // 
             // LogInForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.lbl_logIn);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.Controls.Add(this.lbl_title2);
+            this.Controls.Add(this.lbl_title1);
+            this.Controls.Add(this.btn_logIn);
+            this.Controls.Add(this.txb_password);
+            this.Controls.Add(lbl_password);
+            this.Controls.Add(this.tbx_username);
+            this.Controls.Add(this.lbl_username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LogInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registrarse";
             this.Load += new System.EventHandler(this.LogInForm_Load);
             this.ResumeLayout(false);
@@ -85,9 +136,12 @@
 
         #endregion
 
-        private Label lbl_logIn;
-        private TextBox textBox1;
-        private Label label1;
-        private TextBox textBox2;
+        private Label lbl_username;
+        private TextBox tbx_username;
+        private Label lbl_password;
+        private TextBox txb_password;
+        private Button btn_logIn;
+        private Label lbl_title1;
+        private Label lbl_title2;
     }
 }
